54fd26d79646ba581e5938a692e13c931c59caf7
Ewiels <edward.wiels@student.kuleuven.be>
2021-04-15 11:32:17 +0200

    added lqr_controller as function

--- Seesaw/lqr_controller.m
+++ Seesaw/lqr_controller.m
@@ -1,5 +1,7 @@
-%% Calculate LQR gain K and save it in order to be used in Simulink
-Q = diag([1000,4000,0,0]);
-R=10;
-K = lqr(sys,Q,R);
-save('lqr_K.mat','K');
+% Calculate LQR gain K and save it in order to be used in Simulink
+% Tuning of Q and R happens here
+function K = lqr_controller(sys)
+    Q = diag([1000,4000,0,0]);
+    R=10;
+    K = lqr(sys,Q,R);
+end
