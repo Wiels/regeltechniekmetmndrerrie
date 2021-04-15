f29c5cb2e2e9617937046b66f8fc5ee04b235bb5
Ewiels <edward.wiels@student.kuleuven.be>
2021-04-15 10:24:41 +0200

    dd

---
+++ Quadcopter/main.m
@@ -0,0 +1,9 @@
+%% Load references file
+load('references_08.mat')
+
+%% Open Simulink library
+slLibraryBrowser
+
+%% Generate report
+generate_report(0)
+
---
+++ Seesaw/swmain.m
@@ -0,0 +1,5 @@
+
+%% Open Simulink library
+slLibraryBrowser
+%% Initial conditions
+initial_conditions = [0,0,0,0];
