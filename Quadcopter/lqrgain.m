07cd5087c0f6a6ae0e04f4a3d786c3ad496defa7
Ewiels <edward.wiels@student.kuleuven.be>
2021-05-03 15:20:59 +0200

    hmm

--- Quadcopter/lqrgain.m
+++ Quadcopter/lqrgain.m
@@ -1,10 +1,15 @@
-clear;
+load('references_08.mat')
 load 'discrete_linearized_tustin.mat';
 %% full system (dlqr not usable)
 A = sysd.A;
 B = sysd.B;
 C = sysd.C;
 D = sysd.D;
-Q = eye(12)*0.01;
-R = diag([10,10,1,10]);
+Qx = 1e4;
+Qy = 1e4;
+Qz = 1e6;
+Q = diag([Qx,Qy,Qz,ones(1,9)*0.1]);
+R = eye(4)*10000;
 K = dlqr(A,B,Q,R);
+
+
