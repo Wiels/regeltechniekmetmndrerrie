2b7d04a613cb1194d7fb6686319a40e2c45a0723
Ewiels <edward.wiels@student.kuleuven.be>
2021-05-06 11:46:59 +0200

    lqi in orde precies, trekt wel ipn it

--- Quadcopter/lqi_augmsystem.m
+++ Quadcopter/lqi_augmsystem.m
@@ -1,10 +1,13 @@
 load('references_08.mat')
+load 'sys.mat';
 load 'discrete_linearized_tustin.mat';
 %% 
-Aaug = [diag([1,1,1,zeros(1,3)]),sysd.C;zeros(12,6),sysd.A];
-Baug = [sysd.D;sysd.B];
-Caug = diag([ones(1,3),ones(1,3),zeros(1,12)]);
-Daug = zeros(18,4);
+hs = ss(sys.A,sys.B,[eye(3),zeros(3,9)],zeros(3,4));
+hsd = c2d(hs,0.05,'tustin');
+Aaug = [diag([1,1,1]),hsd.C;zeros(12,3),sysd.A];
+Baug = [hsd.D;sysd.B];
+Caug = diag([ones(1,3),zeros(1,12)]);
+Daug = zeros(15,4);
 sysaug = ss(Aaug,Baug,Caug,Daug,0.05);
 
 
