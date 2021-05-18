e557688a78bdbc24874813bd472c42e8c18f2ba5
martijndebeuf <martijn.debeuf@student.kuleuven.be>
2021-05-18 18:39:35 +0200

    second exercise for report

--- Seesaw/exercise2/ex2main.m
+++ Seesaw/exercise2/ex2main.m
@@ -2,20 +2,21 @@
 clear;
 load exercise2/references2
 %desired angle
-theta_d = pi/90;
+theta_d = 0;
 
 %initial angle and pos
 init_theta_degrees=0;
 init_theta = -pi/(180/init_theta_degrees);
-init_x = 0.0;
+init_x = 0;
 
 
-%% Setup
-    %Q = diag([1000,5000,0,0]);
-    %R = 1e10;
-   Q = diag([1000,4000,0,0]);
-   R = 10;
-dist = [0;pi/(180/110);0;0]
+%% Setup %settings step: r=50, 1000,1e6,0,5*1e4
+    Q = diag([1000,1e6,0,5*1e4]);
+    %Q = diag([1e6,1e6,0,0]);
+    R = 1e4;
+   %Q = diag([0,5000,0,0]);
+   %R = 5*1e1;
+dist = [0;pi/(180/100);0;0]
 params = get_params();
 sys = get_fullstate_system(params);
  K = lqr(sys,Q,R);
